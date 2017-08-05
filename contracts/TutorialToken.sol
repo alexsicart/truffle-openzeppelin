42ec8f29a1000d776fcfd2692f0a69c74dc84011
alexsicart <alex@sharge.io>
2017-08-05 10:00:40 +0200

    fix: node_modules

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -1,14 +1,17 @@
 pragma solidity ^0.4.4;
-import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
+import 'node_modules/zeppelin-solidity/contracts/token/StandardToken.sol';
 
 contract TutorialToken is StandardToken {
+
   string public name = 'TutorialToken';
-  string public symbol = '#';
+  string public symbol = 'TT';
   uint public decimals = 2;
   uint public INITIAL_SUPPLY = 12000;
 
   function TutorialToken() {
-  totalSupply = INITIAL_SUPPLY;
-  balances[msg.sender] = INITIAL_SUPPLY;
-}
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+
+  }
 }
